cec51b31a9598a8320771d059fdbe9d1f832f491
Luqui87 <alvarezlucas2787@gmail.com>
2022-11-28 16:01:54 -0300

    views de items

--- members/urls.py
+++ members/urls.py
@@ -2,5 +2,23 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index')
+    # ex: /members/
+    path('', views.index, name='index'),
+
+# ex: /members/
+    path('aventureros/', views.aventureros, name='aventureros'),
+
+    path('aventureros/add/', views.addAventurero, name='addAventurero'),
+
+    path('aventureros/add/addrecord/', views.addAventureroPost, name='addAventurero'),
+
+    # ex: /members/aventureros/5/
+    path('aventureros/<int:aventurero_id>', views.aventurero, name= 'aventurero'),
+
+    path('aventureros/delete/<int:id>', views.delete, name='delete'),
+
+    #ex: /members/item/1/
+    path('items/<int:item_id>', views.item, name = 'item'),
+#ex: /members/item/1/
+    path('items/', views.items, name = 'items')
 ]
--- members/views.py
+++ members/views.py
@@ -1,6 +1,49 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.template import loader
+from .models import Aventurero, Item
+from django.urls import reverse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello world")
+    template = loader.get_template('index.html')
+    return HttpResponse(template.render())
+
+def aventureros(request):
+    aventureros = Aventurero.objects.all().values()
+    # aventureros = Aventurero.objects.raw('SELECT * FROM members_aventurero')
+    template = loader.get_template('aventureros.html')
+    context = {
+        'aventureros':aventureros
+    }
+    return HttpResponse(template.render(context, request))
+
+def addAventureroPost(request):
+    x = request.POST['nombre']
+    y = request.POST['vida']
+    aventurero = Aventurero(nombre=x, vida=y)
+    aventurero.save()
+    return HttpResponseRedirect(reverse ('aventureros'))
+
+def delete(request,id):
+    aventurero = Aventurero.objects.get(id=id)
+    aventurero.delete()
+    return HttpResponseRedirect(reverse('aventureros'))
+
+def aventurero(request, aventurero_id):
+    return HttpResponse("Estas son las stats del aventurero %s." % aventurero_id)
+
+def item(request, item_id):
+    return HttpResponse("Estas son las stats del item %s" % item_id)
+
+def addAventurero(request):
+    template = loader.get_template('addAventurero.html')
+    return HttpResponse(template.render({},request))
+
+def items(request,):
+    items = Item.objects.all().values()
+    template = loader.get_template('items.html')
+    context = {
+        'items' : items
+    }
+    return HttpResponse(template.render(context,request))
